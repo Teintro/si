fae0ac0cf99b7b1988ff120fb84c3ed510472d01
Teintro <127615450+Teintro@users.noreply.github.com>
2023-08-19 13:12:42 -0500

    Add files via upload

--- Prueba/index.js
+++ Prueba/index.js
@@ -1,15 +1,24 @@
-function tarjeta() {
-    const card = document.createElement("div")
-    card.classList.add("card")
+const container = document.getElementById("container")
+
+function card() {
+    const cardc = document.createElement('div');
+    cardc.classList.add("card")
     
-    const cardtittle = document.createElement("h1")
-    cardtittle.textContent = "hey";
+    const cardTitle = document.createElement("h2");
+    cardTitle.textContent = 'Hola';
 
-    const cardcontent = document.createElement("p");
-    cardcontent.textContent = "buenas noches";
+    const cardImg = document.createElement("img")
+    cardImg.src = "https://img-16.stickers.cloud/packs/fe1ed939-39f5-42fa-b100-3df06c84ef03/webp/cb5e30d3-4d9a-477e-869f-ece31f2fded8.webp"
+    cardImg.style.width = "100%"
 
-    card.appendChild(cardtittle);
-    card.appendChild(cardcontent);
+    const cardContent = document.createElement("p");
+    cardContent.textContent  = "CHOKOCRISPIS"
 
-    card.appendChild(card);
+
+    cardc.appendChild(cardTitle);
+    cardc.appendChild(cardImg)
+    cardc.appendChild(cardContent);
+
+    container.appendChild(cardc)
 }
+
